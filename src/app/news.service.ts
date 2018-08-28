1b19901657ae99ed0ce1ea784d56b28c9d3874f7
Eric Perez <ebperez@unomaha.edu>
2018-08-28 13:18:00 -0500

    Quick change

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -7,7 +7,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class NewsService {
 
   private newsApiKey = 'd509c5c262424c3fae66e253655fcd68';
-  private newsApiTopUrl = 'https://newsapi.org/v2/top-headlines'
+  private newsApiTopUrl = 'https://newsapi.org/v2/top-headlines';
 
   constructor(
     private http: HttpClient
