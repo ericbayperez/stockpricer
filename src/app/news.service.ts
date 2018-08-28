cf75ed049af62648607063c8b6cf4c02b9994c67
Eric Perez <ebperez@unomaha.edu>
2018-08-28 13:17:31 -0500

    Added in news and stock services (unwritten) along with a home component

---
+++ src/app/news.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  private newsApiKey = 'd509c5c262424c3fae66e253655fcd68';
+  private newsApiTopUrl = 'https://newsapi.org/v2/top-headlines'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+}
